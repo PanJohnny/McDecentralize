b2d7a9b247f0ab6866a7ddcc686657d9fd30eb22
Jan Štefanča <janstefanca@seznam.cz>
2025-03-01 16:12:51 +0100

    Bump versions

--- src/main/java/me/panjohnny/mcdec/McDecentralize.java
+++ src/main/java/me/panjohnny/mcdec/McDecentralize.java
@@ -4,10 +4,9 @@ import me.panjohnny.mcdec.commands.*;
 import me.panjohnny.mcdec.config.ConfigKeyRegistry;
 import picocli.CommandLine;
 
-import java.nio.file.Path;
 import java.util.concurrent.Callable;
 
-@CommandLine.Command(name = "mcdec", mixinStandardHelpOptions = true, version = "MCDecentralize 1.0", subcommands = {Init.class, Sync.class, Run.class, Share.class, Config.class})
+@CommandLine.Command(name = "mcdec", mixinStandardHelpOptions = true, version = "1.2", subcommands = {Init.class, Sync.class, Run.class, Share.class, Config.class})
 public class McDecentralize implements Callable<Integer> {
     public static final String ART = """
             _  _ ____ ___  ____ ____ ____ _  _ ___ ____ ____ _    _ ___  ____
@@ -20,14 +19,6 @@ public class McDecentralize implements Callable<Integer> {
     public static String path(String path) {
         if (!relativePath.isBlank()) {
             return relativePath + "/" + path;
-        } else {
-            return path;
-        }
-    }
-
-    public static Path relativizePath(Path path) {
-        if (!relativePath.isBlank()) {
-            return Path.of(relativePath).relativize(path);
         } else {
             return path;
         }
